2e6cab4238f6ad33ef2a6e9299e5a73ffa5b9f8a
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2008-03-07 20:12:04 -0300

    Add myself as author of id3

--- src/plugins/id3/id3.c
+++ src/plugins/id3/id3.c
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  * @author Andre Moreira Magalhaes <andre.magalhaes@openbossa.org>
+ * @author Gustavo Sverzut Barbieri <gustavo.barbieri@openbossa.org>
  */
 
 /**
