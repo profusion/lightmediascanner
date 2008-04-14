9394c1cd87a95333b35bb5783df77678120d6c61
Eduardo Lima (Etrunko) <eduardo.lima@indt.org.br>
2008-04-14 16:17:49 -0300

    Fixed FSF address.

--- src/plugins/ogg/ogg.c
+++ src/plugins/ogg/ogg.c
@@ -13,9 +13,11 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * @author Renato Chencarek <renato.chencarek@openbossa.org>
+ * @author Eduardo Lima (Etrunko) <eduardo.lima@indt.org.br>
+ *
  */
 
 /**
