515006cd5ffed1339e37a46332969f758e707413
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2011-08-19 17:54:37 -0300

    improve autofoo handling of m4 and generation.
    Use recommended macro in configure.ac and Makefile.am to define "m4/"
    as our storage for macro files.
    
    Use autoreconf instead of manually running all the commands on our
    own.

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 
-# Allow invocation from a separate build directory; in that case, we change
-# to the source directory to run the auto*, then change back before running configure
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
+origdir=`pwd`
 
-DIRNAME=`basename $PWD`
-if test "x$DIRNAME" = "xelementary-skel"; then
-    echo "Do not run autogen.sh from inside elementary-skel"
-    exit 1
-fi
-
-rm -rf autom4te.cache
-rm -f aclocal.m4 ltmain.sh
-
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
-echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
-
-cd $ORIGDIR || exit 1
+cd "$srcdir"
+autoreconf --force --install -I m4/
+cd "$origdir"
 
 if [ -z "$NOCONFIGURE" ]; then
     $srcdir/configure $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
