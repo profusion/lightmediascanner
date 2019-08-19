profusion/lightmediascanner

commit 85fd21df645dbc519d192fc0ebdb0051134c1fb3

Make realloc() usage more robust.
Always check the realloc() return in a different variable and take a
more reasonable approach when it fails.

src/lib/lightmediascanner_db_common.c
@@ -404,17 +404,16 @@ lms_db_cache_find_db(const struct lms_db_cache *cache, const sqlite3 *db)
 }
 
 static int
-lms_db_cache_resize(struct lms_db_cache *cache, int new_size)
+lms_db_cache_resize(struct lms_db_cache *cache, unsigned int new_size)
 {
-    cache->size = new_size;
-    cache->entries = realloc(cache->entries,
-                             cache->size * sizeof(*cache->entries));
-    if (cache->size && !cache->entries) {
+    void *tmp = realloc(cache->entries,
+                        cache->size * sizeof(*cache->entries));
+    if (new_size > 0 && !tmp) {
         perror("realloc");
-        cache->size = 0;
         return -1;
     }
-
+    cache->size = new_size;
+    cache->entries = tmp;
     return 0;
 }
 

src/lib/lightmediascanner.c
@@ -292,6 +292,7 @@ lms_plugin_t *
 lms_parser_add(lms_t *lms, const char *so_path)
 {
     struct parser *parser;
+    void *tmp;
 
     if (!lms)
         return NULL;
@@ -304,12 +305,13 @@ lms_parser_add(lms_t *lms, const char *so_path)
         return NULL;
     }
 
-    lms->parsers = realloc(lms->parsers,
-                           (lms->n_parsers + 1) * sizeof(struct parser));
-    if (!lms->parsers) {
+    tmp = realloc(lms->parsers,
+                  (lms->n_parsers + 1) * sizeof(struct parser));
+    if (!tmp) {
         perror("realloc");
         return NULL;
     }
+    lms->parsers = tmp;
 
     parser = lms->parsers + lms->n_parsers;
     if (_parser_load(parser, so_path) != 0) {
@@ -378,18 +380,17 @@ lms_parser_del_int(lms_t *lms, int i)
         return 0;
     } else {
         int dif;
+        void *tmp;
 
         dif = lms->n_parsers - i;
         if (dif)
             memmove(parser, parser + 1, dif * sizeof(struct parser));
 
-        lms->parsers = realloc(lms->parsers,
-                               lms->n_parsers * sizeof(struct parser));
-        if (!lms->parsers) {
-            lms->n_parsers = 0;
+        tmp = realloc(lms->parsers,
+                      lms->n_parsers * sizeof(struct parser));
+        if (!tmp)
             return -1;
-        }
-
+        lms->parsers = tmp;
         return 0;
     }
 }

src/lib/lightmediascanner_charset_conv.c
@@ -293,13 +293,20 @@ _conv(iconv_t cd, char **p_str, unsigned int *p_len, char *ostr, unsigned int ol
     *p_str = ostr;
 
     outbuf = realloc(*p_str, *p_len + 1);
-    if (!outbuf)
-        perror("realloc");
-    else
+    if (outbuf)
         *p_str = outbuf;
+    else {
+        perror("realloc");
+        if (*p_len > 0)
+            (*p_len)--;
+        else {
+            free(*p_str);
+            *p_str = NULL;
+            return 0;
+        }
+    }
 
     (*p_str)[*p_len] = '\0';
-
     return 0;
 }
 

commit 44a27140fa26036636b3791a54699a732b5dbd4a

Fix a regression in lms_db_cache_resize
In commit 85fd21df645dbc519d192fc0ebdb0051134c1fb3,
the assigning of the value of new_size to cache->size was deferred for
more robust error handling. However, cache->size is still used for
size calculation for realloc, not new_size.

src/lib/lightmediascanner_db_common.c
@@ -407,7 +407,7 @@ static int
 lms_db_cache_resize(struct lms_db_cache *cache, unsigned int new_size)
 {
     void *tmp = realloc(cache->entries,
-                        cache->size * sizeof(*cache->entries));
+                        new_size * sizeof(*cache->entries));
     if (new_size > 0 && !tmp) {
         perror("realloc");
         return -1;